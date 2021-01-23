8a929c429864a1f654e8968e8a1875b4489b4b03
Sergey Savrukhin <savrawar@mail.ru>
2021-01-23 06:06:07 +0300

    added react app

---
+++ src/main/java/com/hvdbs/awsimageupload/profile/UserProfileService.java
@@ -0,0 +1,30 @@
+package com.hvdbs.awsimageupload.profile;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class UserProfileService {
+    private final UserProfileDataAccessService userProfileDataAccessService;
+
+    @Autowired
+    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService) {
+        this.userProfileDataAccessService = userProfileDataAccessService;
+    }
+
+    List<UserProfile> getUserProfiles() {
+        return userProfileDataAccessService.getUserProfiles();
+    }
+
+    public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
+        //1. Check if image is not empty
+        //2. If file is an image
+        //3. The user exists in our database
+        //4. Grab some metadata from file if any
+        //5. Store the image in s3 and update database (userProfileImageLink) with s3 image link
+    }
+}
